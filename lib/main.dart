928cd6e1243f93e9f1bd1ab262df053b8316f3a8
uedazxcvbnm <kd1325559@st.kobedenshi.ac.jp>
2025-01-28 13:13:42 +0900

    コメント0128_1

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,8 @@ class ImagePredictionScreen extends StatefulWidget {
 
     Future<void> _loadModel() async {
       try {
-        // _interpreter = await Interpreter.fromAsset('assets/model.tflite');
-        _interpreter = await Interpreter.fromAsset('assets/model_transfer.tflite');
+        _interpreter = await Interpreter.fromAsset('assets/model.tflite');
+        // _interpreter = await Interpreter.fromAsset('assets/model_transfer.tflite');
         // 出力詳細を取得
         var outputDetails = _interpreter.getOutputTensor(0);
         
@@ -84,10 +84,12 @@ class ImagePredictionScreen extends StatefulWidget {
     Float32List inputBuffer = _imageToFloat32List(resizedImage);
 
     // 入力データを [1, 3, 224, 224] の形状にリシェイプ
+    // _printInputShape()での表示結果をもとに配列の変更
     var inputTensor = inputBuffer.reshape([1, 3, 224, 224]);
 
     // 出力形状を取得して出力バッファを作成
     var outputShape = _interpreter.getOutputTensor(0).shape;
+    // 
     var outputBuffer = List.filled(outputShape.reduce((a, b) => a * b), 0.0).reshape(outputShape);
     // var outputBuffer = List.generate(outputShape[0], (_) => List.filled(outputSize, 0.0));
 
@@ -99,42 +101,45 @@ class ImagePredictionScreen extends StatefulWidget {
 
     // 最大値を持つインデックスを取得
     // 出力バッファをフラットなリストに変換
-  // outputBuffer[0]の型に基づいて適切に展開
-  print('autoputtobaffa');
-  // print(outputBuffer[0]); 
-  print(outputBuffer[0].runtimeType); 
-  List<double> outputScores = (outputBuffer[0] as List)  // List<dynamic>からListにキャスト
-    .map((x) {
-      if (x is Iterable) {
-        return x.expand((y) => (y is Iterable) ? y : [y]).toList();  // 2次元目も展開
-      } else {
-        return [x]; // xがIterableでない場合はリストにラップ
-      }
-    })
-    .expand((x) => x is Iterable ? x : [x]) // xがIterableでない場合でもリストとして処理
-    .map((item) => item is double ? item : 0.0)  // itemをdouble型に変換
-    .toList();
+    // outputBuffer[0]の型に基づいて適切に展開
+    print('autoputtobaffa');
+    // print(outputBuffer[0]); 
+    print(outputBuffer[0].runtimeType); 
 
 
-  print('outputscores:');
+    // 
+    List<double> outputScores = (outputBuffer[0] as List)  // List<dynamic>からListにキャスト
+      .map((x) {
+        if (x is Iterable) {
+          return x.expand((y) => (y is Iterable) ? y : [y]).toList();  // 2次元目も展開
+        } else {
+          return [x]; // xがIterableでない場合はリストにラップ
+        }
+      })
+      .expand((x) => x is Iterable ? x : [x]) // xがIterableでない場合でもリストとして処理
+      .map((item) => item is double ? item : 0.0)  // itemをdouble型に変換
+      .toList();
 
-  print(outputBuffer);
-  print(outputShape);
-  print(outputScores);
+
+    print('outputscores:');
+
+    print(outputBuffer);
+    print(outputShape);
+    print(outputScores);
 
 
 
-  // 最大値を持つインデックスを取得（クラス予測）
-  int predictedIndex = outputScores.indexOf(outputScores.reduce((a, b) => a > b ? a : b));
+    // 最大値を持つインデックスを取得（クラス予測）
+    int predictedIndex = outputScores.indexOf(outputScores.reduce((a, b) => a > b ? a : b));
 
-  setState(() {
-    _predictionResult = "Predicted Class: $predictedIndex";
-  });
+    setState(() {
+      _predictionResult = "Predicted Class: $predictedIndex";
+    });
 
 
 
     print("Prediction complete: Class $predictedIndex");
-}
+  }
 
 
 
@@ -160,15 +165,22 @@ void _printInputShape() {
 
       for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
+          // int pixel = って事前に指定しても、どうしても変数pixelは型がピクセル型に自動的になってしまう
           // Pixel型を取得
+          // ここの段階で整数型にする必要はなかった。正規化をする際に変数r, g, bが整数型になっていたらいい
           var pixel = image.getPixel(x, y);
 
           // 赤、緑、青のチャンネルを取得
+          // Pixel 型のメソッドを使用
+          // pixel型から赤、緑、青それぞれのチャンネルを取得
+          // いくら変数の先頭にintをつけていようが意味がない、pixel.rを実行したら返す値はnum型固定だった
+          // なので末尾に.toInt()をつける
           int r = pixel.r.toInt();  // 赤チャンネル
           int g = pixel.g.toInt();  // 緑チャンネル
           int b = pixel.b.toInt();  // 青チャンネル
 
           // 正規化
+          // 正規化をする際に変数r, g, bが整数型になっていたらいい
           floatList[indexR++] = r / 255.0;
           floatList[indexG++] = g / 255.0;
           floatList[indexB++] = b / 255.0;
